8ee6b58fa223b90dc4eaefa033176295bc741d53
Flavio Barisone <lord.igon@gmail.com>
2017-08-17 23:34:25 +0200

    shell script to create link of simlynk files.

--- install-link.sh
+++ install-link.sh
@@ -1,219 +1,58 @@
-#!/bin/bash
-##############################
-# dotfiles installation script
-#
-# Author: Barisone Flavio
-#
-# Release Date: 31-07-2017
-#
-# Version: 1.0.0
-#
-# Directory structure
-#
-#
-##############################
+#!/usr/bin/env bash
 
 source ./functions.sh
 
-main() {
+DOTFILES=$HOME/.dotfiles
 
-  # Get the dotfiles directory's absolute path
-  SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-  DOTFILES_DIR="$(dirname "${SCRIPT_DIR}")"
-  # Example:
-  # SCRIPT_DIR:   /Users/mario/dotfiles
-  # DOTFILES_DIR: /Users/mario/
-  DOTFILE_DIR_NAME=dotfiles
-  DOTFILE_DIR_BACKUP_NAME=dotfiles_bck
-
-  # -n parameter do not actually make any filesystem changes
-  declare -rx stow="stow -n"
-
-  print_info "${SCRIPT_DIR}"
-
-  export SCRIPT_DIR
-  export DOTFILES_DIR
-
-  # Check that DOTFILES_DIR
-  #if ! [ "${DOTFILES_DIR}" = "${HOME}/${DOTFILE_DIR_NAME}" ]; then
-  #  print_error "Script root directory must be ${HOME}/${DOTFILE_DIR_NAME}."
-  #  exit 1
-  #fi
-
-  # Check that git is installed
-  command -v git &>/dev/null || {
-    print_error "Git must be installed in you machine. dotfiles installation stopped."
-    exit 1
-  }
-
-  # Check that nc (netcat utility) is installed
-  command -v nc &>/dev/null || {
-    print_error "nc command must be installed in you machine. dotfiles installation stopped."
-  }
-
-  # Check if computer is connected to Internet
-  echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 &>/dev/null
-  if ! [ $? -eq 0 ]; then
-    print_error "Machine must be connected to internet to download dotfiles."
-    exit 1
-  fi
-
-  DARWIN=false
-  LINUX=false
-
-  # Check Operating System
-  if test "$(uname)" = "Darwin"; then
-    DARWIN=true
-    LINUX=false
-  else
-  #if [ test "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-    DARWIN=false
-    LINUX=true
-  fi
-
-  export DARWIN
-  export LINUX
-
-  # Check for Homebrew
-  if test ! $(which brew)
-  then
-    print_info "Installing Homebrew."
-
-    # Install the correct homebrew for each OS type
-    if $DARWIN
-    then
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    elif $LINUX
-    then
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+print_info "\nCreating symlinks"
+print_info "=============================="
+linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+for file in $linkables ; do
+    target="$HOME/.$( basename $file '.symlink' )"
+    if [ -e $target ]; then
+        print_question "~${target#$HOME} already exists... Skipping."
+    else
+        print_info "Creating symlink for $file"
+        ln -s $file $target
     fi
-  fi
+done
 
-  dotfiledir="${HOME}/{${DOTFILES_DIR}"                        # dotfiles directory
-  dotfiledir_bck="${HOME}/${DOTFILE_DIR_BACKUP_NAME}"             # old dotfiles backup directory
+print_info -e "\n\ninstalling to ~/.config"
+print_info "=============================="
+if [ ! -d $HOME/.config ]; then
+    print_info "Creating ~/.config"
+    mkdir -p $HOME/.config
+fi
 
-  # Backup file esistenti
-  # Create dotfiles_old in homedir
-  print_info "Creating ${dotfiledir_bck} for backup of any existing dotfiles in ${HOME}..."
-  mkdir -p "${dotfiledir_bck}"
-
-  declare -a FILES_TO_SYMLINK=(
-    'zsh/.shell_aliases'
-    'zsh/.shell_config'
-    'zsh/.shell_exports'
-    'zsh/.shell_functions'
-    'zsh/.zshrc'
-    'bash/.bash_profile'
-    'bash/.bash_prompt'
-    'bash/.bashrc'
-    'ack/.ackrc'
-    'curl/.curlrc'
-    'gem/.gemrc'
-    'input/.inputrc'
-    'screen/.screenrc'
-    'git/.gitattributes'
-    'git/.gitconfig'
-    'git/.gitignore_global'
-  )
-
-  print_info "Moving any existing dotfiles from ~ to ${dotfiledir_bck}"
-
-  for i in ${FILES_TO_SYMLINK[@]}; do
-    file=${i##*/}
-    if [ -f ~/${file} ]; then
-      print_info "Moving file" ${file}
-      #mv ~/${i##*/} ${dotfiledir_bck}
+for config in $DOTFILES/config/*; do
+    target=$HOME/.config/$( basename $config )
+    if [ -e $target ]; then
+        print_question "~${target#$HOME} already exists... Skipping."
+    else
+        print_info "Creating symlink for $config"
+        ln -s $config $target
     fi
-  done
+done
 
+# create vim symlinks
+# As I have moved off of vim as my full time editor in favor of neovim,
+# I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
+# as it is not really being actively maintained. However, I would still
+# like to configure vim, so lets symlink ~/.vimrc and ~/.vim over to their
+# neovim equivalent.
 
-  # Update Homebrew recipes
-  print_info "Updating brew"
-  brew update
-  print_info "Upgrading brew"
-  brew upgrade
+print_info "\n\nCreating vim symlinks"
+print_info "=============================="
+VIMFILES=( "$HOME/.vim:$DOTFILES/vim/.vim"
+        "$HOME/.vimrc:$DOTFILES/vim/.vimrc" )
 
-  # Install all our dependencies with bundle (See Brewfile)
-  brew tap homebrew/bundle
-  print_info "Install brew recipes and cask applications"
-  # brew bundle
-
-  brew cleanup
-
-  # Stow dot files..
-  print_info "stow ack file/s"
-  ${stow} ack
-  print_info "stow bash file/s"
-  ${stow} bash
-  print_info "stow curl file/s"
-  ${stow} curl
-  print_info "stow gem file/s"
-  ${stow} gem
-  print_info "stow input file/s"
-  ${stow} input
-  print_info "stow screen file/s"
-  ${stow} screen
-  print_info "stow git file/s"
-  ${stow} git
-  print_info "stow zsh file/s"
-  ${stow} zsh
-  
-  # Add specific application installations. Every folder can contain install.sh file to
-  # install application specific stuff and ._app_aliases file to source specific application alias.
-  declare -a APP_FOLDER=(
-    'docker'
-    #'maven'
-    #'java'
-  )
-
-  for i in ${APP_FOLDER[@]}; do
-    app=${i##*/}
-    app_aliases=".${app}_aliases"
-    folder="${SCRIPT_DIR}/${app}"
-    if [ -d "${folder}" ]; then
-      print_info "${folder}/install.sh"
-      [ -f "./${app}/install.sh" ] && sh -c "./${app}/install.sh"
-      if [ -f "./${app}"/"${app_aliases}" ]; then
-        ${stow} "${app}"
-        # link_file "${folder}"/"${app_aliases}" "${DOTFILES_DIR}"/"${app_aliases}"
-        source_file "${DOTFILES_DIR}"/"${app_aliases}"
-      fi
+for file in "${VIMFILES[@]}"; do
+    KEY=${file%%:*}
+    VALUE=${file#*:}
+    if [ -e ${KEY} ]; then
+        print_question "${KEY} already exists... skipping."
+    else
+        print_info "Creating symlink for $KEY"
+        ln -s ${VALUE} ${KEY}
     fi
-  done
-
-  # Switch to using brew-installed bash as default shell
-  #if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  #  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  #  chsh -s /usr/local/bin/bash;
-  #fi;
-}
-
-
-#### EXECUTE MAIN FUNCTION ####
-main "$@"
-
-
-# Make ZSH the default shell environment
-# chsh -s $(which zsh)
-
-# Install Composer
-# curl -sS https://getcomposer.org/installer | php
-# mv composer.phar /usr/local/bin/composer
-
-# Install global Composer packages
-# /usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw spatie/http-status-check bramus/mixed-content-scan laravel/spark-installer
-
-# Install Laravel Valet
-# $HOME/.composer/vendor/bin/valet install
-
-# Install global NPM packages
-#npm install --global yarn
-
-# Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
-#mkdir $HOME/Sites
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-#source .macos
-
+done
