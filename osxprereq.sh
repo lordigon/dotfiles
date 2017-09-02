6e10cd6f266937f0042341779343d83f466bcda0
Flavio Barisone <flavio.barisone@sap.com>
2017-09-02 18:30:34 +0200

    Delete useless code. Added quotes to XCode directory selection.

--- osxprereq.sh
+++ osxprereq.sh
@@ -1,9 +1,10 @@
-source ./functions.sh
+#!/usr/bin/env bash
+#source ./functions.sh
 
-sudo -v
+#sudo -v
 
 # Keep-alive: update existing 'sudo' time stamp until script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+#while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Step 1: Update the OS and install Xcode tools
 # print_info "Updating OSX. If it requires a restart, run the script again."
@@ -31,7 +32,7 @@ if ! xcode-select --print-path &> /dev/null; then
   # the appropriate directory from within `Xcode.app`
   # https://github.com/alrra/dotfiles/issues/13
 
-  sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+  sudo xcode-select -switch "/Applications/Xcode.app/Contents/Developer"
   print_result $? 'Make "xcode-select" developer directory point to Xcode.'
 
   # Prompt user to agree to the terms of the Xcode license
