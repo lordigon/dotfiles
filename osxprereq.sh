5302b7d247a2ff1f4d7941e568f646875bd34a9c
Flavio Barisone <lord.igon@gmail.com>
2017-12-10 21:51:49 +0100

    Added update of High Sierra Command Line tools.

--- osxprereq.sh
+++ osxprereq.sh
@@ -2,7 +2,6 @@
 
 if ! xcode-select --print-path &> /dev/null; then
 
-  print_info "Installing Command Line Tools"
 
   # Unattended way to install XCode command line tools
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
@@ -13,7 +12,14 @@ if ! xcode-select --print-path &> /dev/null; then
     tr -d '\n')
   softwareupdate -i "$PROD";
 
-  print_result $? "Install Xcode Command Line Tools."
-
   \rm -rf /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
+  # Update Command Line Tools to High Sierra
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD";
+  
 fi
